c008c9c28e13fcd5b32805de0072f801523edc53
shantanu shirbhate <shantanushirbhate07@gmail.com>
2026-03-10 14:00:44 +0530

    commit err fo tolowrcase

--- src/component/mybookings/mybookings.jsx
+++ src/component/mybookings/mybookings.jsx
@@ -264,7 +264,7 @@ const BookeddHospitalCard = ({ bookings, onDelete }) => {
             {/* Hospital Details */}
             <CardContent sx={{ flex: 1 }}>
               <Typography variant="h6" component="h3" sx={{ fontWeight: 600 }}>
-                {booking?.hospitalName.toLowerCase()}
+                {booking?.hospitalName}
               </Typography>
 
               <Typography sx={{ mt: 1 }}>
